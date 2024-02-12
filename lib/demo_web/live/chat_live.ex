3ab01abef72f2a825a5974605332d039efe3c7dc
Steph Crown <emmanuelstephen024@gmail.com>
2024-02-12 16:09:10 +0100

    Separate rendering logic in liveview

--- lib/demo_web/live/chat_live.ex
+++ lib/demo_web/live/chat_live.ex
@@ -1,23 +1,6 @@
 defmodule DemoWeb.ChatLive do
   use Phoenix.LiveView
 
-  def render(assigns) do
-    ~H"""
-    <div>
-      <h1>Chat Messages</h1>
-      <ul>
-        <%= for message <- @messages do %>
-          <li><%= message %></li>
-        <% end %>
-      </ul>
-      <form phx-submit="add-message">
-        <input type="text" name="message" placeholder="Type a message..." />
-        <button type="submit">Add</button>
-      </form>
-    </div>
-    """
-  end
-
   def mount(_params, _session, socket) do
     {:ok, assign(socket, messages: [])}
   end
