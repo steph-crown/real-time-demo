cdab5fa7614774534fd14310570cf71471c26996
Steph Crown <emmanuelstephen024@gmail.com>
2024-02-29 09:46:43 +0100

    Verifying the token in connect/3

--- lib/demo_web/channels/chat_socket.ex
+++ lib/demo_web/channels/chat_socket.ex
@@ -1,5 +1,6 @@
 defmodule DemoWeb.ChatSocket do
   use Phoenix.Socket
+  require Logger
 
   # A Socket handler
   #
@@ -34,8 +35,17 @@ defmodule DemoWeb.ChatSocket do
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
   @impl true
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket, _connect_info) do
+    inspect(token, label: "token")
+
+    case Phoenix.Token.verify(socket, "demo salt", token, max_age: 86_400) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+
+      {:error, reason} ->
+        Logger.error("Failed to connect: #{inspect(reason)}")
+        :error
+    end
   end
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
