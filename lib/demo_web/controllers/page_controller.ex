aec1d60e9f3ec1f3f12f0ff90243c8b8417febaf
Steph Crown <emmanuelstephen024@gmail.com>
2024-03-22 11:56:34 +0100

    Add client connection authentication

--- lib/demo_web/controllers/page_controller.ex
+++ lib/demo_web/controllers/page_controller.ex
@@ -1,9 +1,18 @@
 defmodule DemoWeb.PageController do
   use DemoWeb, :controller
 
+  @user_id 1
+
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    user_token = generate_token(conn, @user_id)
+
+    conn
+    |> assign(:user_token, user_token)
+    |> assign(:user_id, @user_id)
+    |> render(:home, layout: false)
+  end
+
+  defp generate_token(conn, user_id) do
+    Phoenix.Token.sign(conn, "demo salt", user_id)
   end
 end
